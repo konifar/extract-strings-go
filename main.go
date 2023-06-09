7c9c53daf8557c69ef4199ee1ae8ea963b1ea963
konifar <yahpeycoy0403@gmail.com>
2023-06-09 10:25:54 +0900

    文字列を修正

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+	"unicode/utf8"
 )
 
-// go run main.go -dir="/Users/yusuke.konishi/work/github.com/Kyash/platform-api"
-
+// 引数で渡されたパス内のgoファイルの中から文字列定数と該当ファイル行数を出力する
 func main() {
 	// コマンドライン引数からディレクトリのパスを取得
 	dirPath := flag.String("dir", "", "directory path")
@@ -36,13 +36,13 @@ func main() {
 		log.Fatal("Error extracting string constants:", err)
 	}
 
-	// 取得した文字列定数を表示
+	// 取得した文字列定数を出力
 	for _, constant := range constants {
 		fmt.Println(constant)
 	}
 }
 
-// 指定したディレクトリから再帰的にGoファイルのパスを取得する関数
+// 指定したディレクトリから再帰的にGoファイルのパスを取得する
 func findGoFiles(dirPath string) ([]string, error) {
 	var goFiles []string
 	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
@@ -68,16 +68,20 @@ func findGoFiles(dirPath string) ([]string, error) {
 	return goFiles, nil
 }
 
-// 指定したGoファイル群から文字列定数を抽出する関数
+// 指定したGoファイル群から文字列定数を抽出する
 func extractStringConstants(filePaths []string) ([]string, error) {
-	// 文字列定数を格納するスライス
-	constants := make([]string, 0)
+	// 文字列定数と該当箇所を格納する構造体のスライス
+	constantDetails := make([]struct {
+		Constant string
+		FilePath string
+		Line     int
+	}, 0)
 
 	// WaitGroupを使用して並行処理を管理
 	var wg sync.WaitGroup
 	wg.Add(len(filePaths))
 
-	// ファイルごとに解析（並行処理）
+	// ファイルごとに解析
 	for _, filePath := range filePaths {
 		go func(path string) {
 			defer wg.Done()
@@ -111,18 +115,44 @@ func extractStringConstants(filePaths []string) ([]string, error) {
 							continue
 						}
 
-						// 文字列定数をスライスに追加
-						constants = append(constants, basicLit.Value)
+						// 非ASCII文字が含まれているかチェック
+						hasNonASCII := false
+						for _, r := range basicLit.Value {
+							if r >= utf8.RuneSelf {
+								hasNonASCII = true
+								break
+							}
+						}
+
+						if hasNonASCII {
+							// 文字列定数と該当箇所をスライスに追加
+							constantDetails = append(constantDetails, struct {
+								Constant string
+								FilePath string
+								Line     int
+							}{
+								Constant: basicLit.Value,
+								FilePath: path,
+								Line:     fset.Position(basicLit.Pos()).Line,
+							})
+						}
 					}
 				}
 
 				return true
 			})
+
 		}(filePath)
 	}
 
 	// すべての解析が完了するまで待機
 	wg.Wait()
 
-	return constants, nil
+	// 文字列定数と該当箇所を表示
+	constantLines := make([]string, len(constantDetails))
+	for i, detail := range constantDetails {
+		constantLines[i] = fmt.Sprintf("%s:%d: %s", detail.FilePath, detail.Line, detail.Constant)
+	}
+
+	return constantLines, nil
 }
